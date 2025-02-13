a0bd37a06cbdf73614acae3a0122f80f4d8510e1
IvanaFD <ivanafigueroa0936@gmail.com>
2025-02-12 19:01:09 -0600

    metodos sort

--- IGenereicSort.java
+++ IGenereicSort.java
@@ -14,11 +14,11 @@ import java.util.ArrayList;
 
 interface IGenereicSort<T extends Comparable <T>> {
     
-    void InsertionSort (ArrayList<T> list);
-    void MergeSort (ArrayList<T> list);
-    void QuickSort (ArrayList<T> list);
-    void RadixSort (ArrayList<T> list);
-    void CocktailSort (ArrayList<T> list);
+    void insertionSort (ArrayList<T> list);
+    void mergeSort (ArrayList<T> list);
+    
+    void radixSort (ArrayList<T> list);
+    void cocktailSort (ArrayList<T> list);
     
     
 }
---
+++ SortMethods.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class SortMethods<T extends Comparable<T>> implements IGenereicSort<T> {
+    
+    @Override
+    public void insertionSort (ArrayList<T> list){}
+    
+
+    @Override
+    public void mergeSort (ArrayList<T> list){}
+
+    
+
+    @Override
+    public void radixSort (ArrayList<T> list){}
+
+    @Override
+    public void cocktailSort (ArrayList<T> list){}
+}
+    
+
+
